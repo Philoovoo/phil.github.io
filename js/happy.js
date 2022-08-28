2b71d5ca00165d117389862936d5f9ba10350dff
Phil <phil>
2022-08-28 21:55:52 +0800

    8/28

---
+++ js/happy.js
@@ -0,0 +1,35 @@
+$(function () {
+    setTimeout(function () {
+        $('.name').animate({
+            opacity:"1",
+            top:"15%"
+        },2000);
+
+    },6000);
+    setTimeout(function () {
+        $('.happy').animate({
+            opacity:"1",
+            top:"15%"
+        },2000);
+
+    },6000);
+    setTimeout(function () {
+        $('.button-style1').animate({
+            opacity:"1",
+            top:"70%"
+        },1500);
+        $('.button-style2').animate({
+            opacity:"1",
+            top:"85%"
+        },2000);
+    },9000);
+	// $(window).click(function(){
+	// 	$('audio')[0].play();
+	// });
+	// window.audio=$('audio')[0].play();
+	$("#dislike").click(function(event){		
+		alert("什么，你要看？没问题！");
+		location.href="fireworks.html";
+	})
+
+})
